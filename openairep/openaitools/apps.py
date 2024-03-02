ac8d0c41881eb821fee8dd13ebbd990eb2b26646
iknwamah <ikechukwuarinze614@gmail.com>
2024-03-02 02:10:09 +0100

    openai

--- openairep/openaitools/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenaitoolsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'openaitools'
--- openairep/openaitools/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CustomChatGPT
-
-urlpatterns = [
-    path('chat/', CustomChatGPT, name='chat'),
-]
